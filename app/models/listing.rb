ad5781fbf0fe838a7103e1c67d2e4a0b32303a0f
Evrim Cavdar <evrim.cavdar@gmail.com>
2023-03-13 16:20:25 +0100

    filter front-end

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,7 +2,7 @@ class Listing < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many :reviews, through: :bookings
 
-  scope :with_kitchen, -> { where(kitchen: true) }
-  scope :with_private_bathroom, -> { where(private_bathroom: true) }
-  scope :with_laundry, -> { where(laundry: true) }
+  # scope :with_kitchen, -> { where(kitchen: true) }
+  # scope :with_private_bathroom, -> { where(private_bathroom: true) }
+  # scope :with_laundry, -> { where(laundry: true) }
 end
